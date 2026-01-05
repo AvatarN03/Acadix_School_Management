539382792b65a2b5fc3f4ba4eaff013967a839cc
AvatarN03 <jsprashanth003@gmail.com>
2026-01-05 12:26:51 +0530

    fix: files type

--- src/lib/role.ts
+++
@@ -1,7 +0,0 @@
-import { auth } from "@clerk/nextjs/server";
-
-export default async  function assignRole (){
-    const {sessionClaims} = await auth();
-
-    return (sessionClaims?.metadata as {role?:string})?.role;
-}
